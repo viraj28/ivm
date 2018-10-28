8b875efea955015c8ed55b1dd3f62e5860ab7cdd
viraj28 <vnirbhavane28@gmail.com>
2018-10-28 20:35:38 +0530

    Set up Logins for superuser

---
+++ inventory/invent/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InventConfig(AppConfig):
+    name = 'invent'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mi8guy/PycharmProjects/ivm/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
